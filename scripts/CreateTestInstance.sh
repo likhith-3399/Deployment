97de7279b109375e293c85521c38af6b2a7cd05f
Likhith Matta <matta.likhith@gmail.com>
2017-12-19 19:45:18 -0600

    Updating script with parameters

--- scripts/CreateTestInstance.sh
+++ scripts/CreateTestInstance.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 StackName=$1
-wget https://raw.githubusercontent.com/likhith-3399/IntuitCodingChallange/master/src/main/resources/automation/ec2_cloudformation.json
+Region=$2
+CloudFormationTemplateURL=$3
 
-aws cloudformation create-stack --stack-name $StackName --region us-east-1 --template-body file://./ec2_cloudformation.json
+var/lib/jenkins/aws-cli/bin/aws s3 cp cloudformation/ec2_cloudformation.json s3://cloud-formation-templates-001/ec2_cft/ --sse
+/var/lib/jenkins/aws-cli/bin/aws cloudformation create-stack --stack-name $StackName --region $Region --template-url $CloudFormationTemplateURL
+
+/var/lib/jenkins/aws-cli/bin/aws cloudformation describe-stacks --stack-name $StackName > status.json
+while ! grep -q CREATE_COMPLETE status.json
+do
+	sleep 10
+    /var/lib/jenkins/aws-cli/bin/aws cloudformation describe-stacks --stack-name $StackName > status.json
+	echo "Waiting for Cloud Formation to Complete..."
+done
